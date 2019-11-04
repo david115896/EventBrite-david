ebafaaaf1f2dcfbdc1ad5b447c6cca296468fb11
david115896 <dtaccori@gmail.com>
2019-11-04 11:40:42 +0100

    first commit

---
+++ db/migrate/20191104103115_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :email
+      t.string :encrypted_password
+	    t.text :description
+
+      t.string :first_name
+	    t.string :last_name
+      
+      t.belongs_to :city, index: true
+      
+      t.timestamps
+    end
+  end
+end
