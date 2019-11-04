2783c895356a29ffc358f6a2e416553d7978958f
david115896 <dtaccori@gmail.com>
2019-11-04 12:31:23 +0100

    validators

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,6 +69,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.fetch(:user, {})
+      return params.require(:user).permit(:first_name, :email, :encrypted_password )
     end
 end
